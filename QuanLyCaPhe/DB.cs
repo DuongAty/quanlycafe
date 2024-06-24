cb13fc3a6ff772cc1bbe2f2feeaa80c101edffaf
DuongATY <DuongATY@DESKTOP-9OEKM1I>
2024-06-24 18:23:28 +0700

    https://github.com/DuongAty/quanlycafe.git

---
+++ QuanLyCaPhe/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCaPhe
+{
+    static class DB
+    {
+        
+        public static string ConnectString = @"Data Source=DESKTOP-9OEKM1I;Initial Catalog=QLCP;Integrated Security=True";
+    }
+}
